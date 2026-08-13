3296e97cacdb95860933cb5618966cc6f56017a7
Fredo <fredotanzil@gmail.com>
2026-08-13 11:04:53 +0700

    using lombok getter & setter

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -3,8 +3,11 @@ package com.example.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Document(collection = "students")
+@Getter @Setter
 public class Student implements Serializable {
     @Id
     private String id;
@@ -17,12 +20,4 @@ public class Student implements Serializable {
         this.name = name;
         this.email = email;
     }
-
-    // Getters and Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
