8eeb9683e14a3e7f2ef326e7c934ead69ff94b95
Fredo <fredotanzil@gmail.com>
2026-08-19 13:56:00 +0700

    add actuator health check

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.health.actuate.endpoint.HealthEndpoint;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private HealthEndpoint healthEndpoint;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void healthEndpointIsAvailable() {
+		assertThat(healthEndpoint).isNotNull();
+		assertThat(healthEndpoint.health().getStatus()).isNotNull();
 	}
 
 }
